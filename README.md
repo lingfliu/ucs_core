3046a9055987d71a9ed615b82d69092a20030e0d
lingfliu <lingfeng.liu@163.com>
2024-03-25 08:00:09 +0800

    added dispatch

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+admin system administration via rest
+conn：connection management
+data：db operation
+dd: data distribution
+exc: edge-cloud cooperation
+mq: RT message queue
+```
---
+++ dd/data_dispatch.go
@@ -0,0 +1,39 @@
+package dd
+
+type DataDispatch struct {
+	// 用于存储数据的通道
+	source       chan byte
+	dispatchChan chan map[string]chan byte
+}
+
+func NewDataDispatch(source chan byte) *DataDispatch {
+	return &DataDispatch{
+		source:       source,
+		dispatchChan: make(chan map[string]chan byte),
+	}
+}
+
+func (dd *DataDispatch) Reg(name string, ch chan byte) {
+	dd.dispatchChan <- map[string]chan byte{name: ch}
+}
+
+func (dd *DataDispatch) Run() {
+	for {
+		select {
+		case data := <-dd.source:
+			for _, ch := range <-dd.dispatchChan {
+				ch <- data
+			}
+		}
+	}
+}
+
+func (dd *DataDispatch) Unreg(name string) {
+	//remove the channel from the map
+	dd.dispatchChan <- map[string]chan byte{name: nil}
+}
+
+func (dd *DataDispatch) Close() {
+	close(dd.source)
+	close(dd.dispatchChan)
+}
