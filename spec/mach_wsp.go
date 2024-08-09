eac2d68cc6be38e02869a3b1873fa978a2ce698c
lingfliu <lingfeng.liu@163.com>
2024-08-10 00:22:56 +0800

    ADD WSP machine
    see ucs_dev_doc/PROTO.md for explain

--- spec/mach_wsp.go
+++ spec/mach_wsp.go
@@ -2,14 +2,12 @@ package spec
 
 import (
 	"github.com/lingfliu/ucs_core/coder"
-	"github.com/lingfliu/ucs_core/conn"
 )
 
 type MachWspCoder struct {
 }
 
 type MachWspConnCli struct {
-	conn.Cli
 }
 
 func (m *MachWspConnCli) Test() {
