f39594088f025e35aced39f89fa3e414d72d9817
siyu-lii <18779400905@163.com>
2024-11-04 18:44:45 +0800

    update const

--- model/meta/data_meta.go
+++ model/meta/data_meta.go
@@ -3,19 +3,24 @@ package meta
 import "encoding/binary"
 
 const (
-	DATA_CLASS_RAW   = 0 //raw bytes or ASCII byte strings
-	DATA_CLASS_INT8   = 1
-	DATA_CLASS_UINT8   = 2
-	DATA_CLASS_INT16   = 3
-	DATA_CLASS_UINT16   = 4
-	DATA_CLASS_INT32   = 5
-	DATA_CLASS_UINT32   = 6
-	DATA_CLASS_INT64  = 7
-	DATA_CLASS_UINT64  = 8
-	DATA_CLASS_FLOAT = 9
-	DATA_CLASS_DOUBLE = 10
-	DATA_CLASS_FLAG  = 11
-	DATA_CLASS_JSON  = 12 //UTF-8 format json string (used in for url fetching)
+	// DATA_CLASS_RAW   = 0 //raw bytes or ASCII byte strings
+	// DATA_CLASS_INT8   = 1
+	// DATA_CLASS_UINT8   = 2
+	// DATA_CLASS_INT16   = 3
+	// DATA_CLASS_UINT16   = 4
+	// DATA_CLASS_INT32   = 5
+	// DATA_CLASS_UINT32   = 6
+	// DATA_CLASS_INT64  = 7
+	// DATA_CLASS_UINT64  = 8
+	// DATA_CLASS_FLOAT = 9
+	// DATA_CLASS_DOUBLE = 10
+	// DATA_CLASS_FLAG  = 11
+	// DATA_CLASS_JSON  = 12 //UTF-8 format json string (used in for url fetching)
+	DATA_CLASS_RAW   = 0 //raw bytes
+	DATA_CLASS_INT   = 1
+	DATA_CLASS_UINT  = 2
+	DATA_CLASS_FLOAT = 3
+	DATA_CLASS_FLAG  = 4
 )
 
 /**
@@ -26,14 +31,14 @@ type DataMeta struct {
 	Dimen     int
 	SampleLen int
 	Alias     string //代号
-	Code string //代码
+	Code      string //代码
 	Unit      string //单位
 	DataClass int
 	Msb       bool
 }
 
 //TODO: remove on release
-func (meta *DataMeta) Convert(raw []byte) []any {
+func (meta *DataMeta) Convert(raw []byte, byteLen int, dimen int, class int) []any {
 	data := make([]any, dimen)
 	switch class {
 	case DATA_CLASS_RAW:
