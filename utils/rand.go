fa0db740a3996ebffe5a544ead7377ae37257d0d
siyu-lii <18779400905@163.com>
2024-11-22 11:14:27 +0800

    Add RandFloat32

--- utils/rand.go
+++ utils/rand.go
@@ -9,3 +9,7 @@ func RandInt64(min int64, max int64) int64 {
 func RandInt32(min int32, max int32) int32 {
 	return min + rand.Int31()%(max-min)
 }
+
+func RandFloat32(min float32, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
